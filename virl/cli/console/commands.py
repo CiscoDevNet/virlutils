12023be4f53260dcd9cc579e641b8dd4bfcf872b
kecorbin <kecorbin@cisco.com>
2017-12-08 12:48:01 -0600

    add env support for console

--- virl/cli/console/commands.py
+++ virl/cli/console/commands.py
@@ -4,14 +4,21 @@ from subprocess import call
 from virl import helpers
 
 @click.command()
-@click.argument('env', default='default')
-@click.argument('node', default=None, required=False)
-def console(env, node):
+@click.argument('node', nargs=-1)
+def console(node):
     """
     console for node
     """
-    print env, node
-    running = helpers.check_sim_running(env)
+    if len(node) == 2:
+        # we received env and node name
+        env = node[0]
+        running = helpers.check_sim_running(env)
+        node = node[1]
+    else:
+        # assume default env
+        env = 'default'
+        running = helpers.check_sim_running(env)
+        node = node[0]
     if running:
 
         sim_name = running
@@ -19,7 +26,10 @@ def console(env, node):
         resp = server.get_node_console(sim_name, node=node)
         if node:
             print "Attempting to connect to console of {}".format(node)
-            ip, port = resp.json()[node].split(':')
-            exit(call(['telnet', ip, port]))
+            try:
+                ip, port = resp.json()[node].split(':')
+                exit(call(['telnet', ip, port]))
+            except AttributeError:
+                click.secho("Could not find console info for {}:{}".format(env,node), fg="red")
         else:
             return resp.json()
