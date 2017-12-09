8dce0dbf6e2f54a89709b43ea98fe8e3aa17d842
kecorbin <kecorbin@cisco.com>
2017-12-09 15:45:12 -0600

    contextualize virl ls to current working directory
    add --all flag to display all running sims for the user

--- virl/cli/ls/commands.py
+++ virl/cli/ls/commands.py
@@ -1,12 +1,26 @@
+import os
 import click
 from virl.api import VIRLServer
 from virl.cli.views import sim_list_table
 
+
 @click.command()
-def ls(**kwargs):
+@click.option('--all/--local', default=False, help=" \
+Display all simulations or only ones from the current project (default)")
+def ls(all, **kwargs):
     """
-    lists running simulations
+    lists running simulations in the current project
     """
+
     server = VIRLServer()
+
     sim_dict = server.list_simulations()
+    if not all:
+        # only sims for this project
+        dirpath = os.getcwd()
+        foldername = os.path.basename(dirpath)
+        for k in sim_dict.keys():
+            if not k.startswith(foldername):
+                sim_dict.pop(k)
+
     sim_list_table(sim_dict)
