166234905430e9bfc46eb51e57eff537f6285284
Kevin Corbin <kecorbin@cisco.com>
2018-03-02 10:36:15 -0600

    make telnet/ssh consistent

--- virl/cli/telnet/commands.py
+++ virl/cli/telnet/commands.py
@@ -14,30 +14,34 @@ def telnet(node):
         env = node[0]
         running = helpers.check_sim_running(env)
         node = node[1]
-    else:
+    elif len(node) == 1:
         # assume default env
         env = 'default'
         running = helpers.check_sim_running(env)
         node = node[0]
+    else:
+        exit(call(['virl', 'ssh', '--help']))
+
     if running:
         sim_name = running
         server = VIRLServer()
         details = server.get_sim_roster(sim_name)
         #resp = server.get_node_console(sim_name, node=node)
-        for node_dict in details.values():
-            node_name = node_dict.get("NodeName")
-            if node_name == node:
-                click.secho("Attemping telnet connection to {}".format(node_name))
-
         if node:
-
             try:
                 for k,v in details.items():
                     if k.endswith(node):
                         ip = details[k]['managementIP']
+                        for node_dict in details.values():
+                            node_name = node_dict.get("NodeName")
+                            if node_name == node:
+                                click.secho("Attemping telnet connection to {} at {}".format(node_name, ip))
+
                         exit(call(['telnet', ip]))
+
             except AttributeError:
-                click.secho("Could not find console info for {}:{}".format(env,node), fg="red")
+                click.secho("Could not find management info for {}:{}".format(env,node), fg="red")
+
             except KeyError:
                 click.secho("Unknown node {}:{}".format(env,node), fg="red")
         else:
