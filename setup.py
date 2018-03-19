5783c429b5d65eefcaa06d92b4f5db7f50579afc
Kevin Corbin <kecorbin@cisco.com>
2018-03-19 10:25:02 -0500

    releast 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "virlutils"
-VERSION = "0.3"
+VERSION = "0.4"
 # To install the library, run the following
 #
 # python setup.py install
