eaabcac642660651326bcb375df7031b5d9db72d
Kevin Corbin <kecorbin@cisco.com>
2018-04-09 16:14:38 -0500

    remove unused class from swagger.app

--- virl/swagger/app.py
+++ virl/swagger/app.py
@@ -5,23 +5,6 @@ import gunicorn.app.base
 from gunicorn.six import iteritems
 
 
-class StandaloneApplication(gunicorn.app.base.BaseApplication):
-
-    def __init__(self, app, options=None):
-        self.options = options or {}
-        self.application = app
-        super(StandaloneApplication, self).__init__()
-
-    def load_config(self):
-        config = dict([(key, value) for key, value in iteritems(self.options)
-                       if key in self.cfg.settings and value is not None])
-        for key, value in iteritems(config):
-            self.cfg.set(key.lower(), value)
-
-    def load(self):
-        return self.application
-
-
 app = Flask(__name__)
 
 
@@ -37,8 +20,3 @@ def swagger_def(name=None):
     host = os.environ['VIRL_SWAGGER_HOST']
     port = os.environ['VIRL_SWAGGER_PORT']
     return render_template('swagger.json', host=host, port=port)
-
-
-if __name__ == "__main__":
-
-    app.run()
