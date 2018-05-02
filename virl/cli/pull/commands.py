8de2312884ccca5c07a62844d31f25ee3527269c
Kevin Corbin <kecorbin@cisco.com>
2018-05-02 11:19:30 -0500

    handle repo not found

--- virl/cli/pull/commands.py
+++ virl/cli/pull/commands.py
@@ -12,7 +12,10 @@ def pull(repo, **kwargs):
     url = "https://raw.githubusercontent.com/"
     url = url + "{}/master/topology.virl".format(repo)
     resp = requests.get(url)
-
-    with open('topology.virl', 'w') as fh:
-        fh.write(resp.text)
-    click.secho("Saved topology as topology.virl", fg="green")
+    if resp.ok:
+        with open('topology.virl', 'w') as fh:
+            fh.write(resp.text)
+        click.secho("Saved topology as topology.virl", fg="green")
+    else:
+        click.secho("Error pulling {} - repo not found".format(repo),
+                    fg="red")
