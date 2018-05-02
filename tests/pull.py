68cc97a35f25e73eb3ef5692efd4cb9b1ae78ef8
Kevin Corbin <kecorbin@users.noreply.github.com>
2018-05-02 12:16:27 -0500

    handle repo not found (#36)

--- tests/pull.py
+++ tests/pull.py
@@ -17,3 +17,16 @@ class Tests(BaseTest):
             runner = CliRunner()
             result = runner.invoke(virl, ["pull", "foo/bar"])
             self.assertEqual(0, result.exit_code)
+
+    def test_virl_pull_invalid_repo(self):
+        with requests_mock.mock() as m:
+            # Mock the request to return what we expect from the API.
+
+            topo_url = 'https://raw.githubusercontent.com/'
+            topo_url += 'doesnt/exist/master/topology.virl'
+            m.get(topo_url, status_code=400)
+            runner = CliRunner()
+            result = runner.invoke(virl, ["pull", "doesnt/exist"])
+            expected = "Pulling from doesnt/exist\nError pulling " \
+                       "doesnt/exist - repo not found\n"
+            self.assertEqual(result.output, expected)
