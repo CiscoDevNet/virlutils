8d70cb75be791dc9f024e57dc7364444fd04a9be
Joe Clarke <jclarke@cisco.com>
2020-05-07 11:49:39 -0400

    Add "console" command support.
    Note: this doesn't work yet as passing commands on the SSH line results
    in session termination.

--- virl/cli/console/commands.py
+++ virl/cli/console/commands.py
@@ -2,22 +2,63 @@ import click
 from virl.api import VIRLServer
 from subprocess import call
 from virl import helpers
-from virl.cli.views.console import console_table
+from virl.helpers import get_cml_client, get_current_lab, safe_join_existing_lab
+from virl.cli.views.console import console_table, console_table1
 import platform
 
-@click.command()
-@click.argument('node', nargs=-1)
-@click.option('--display/--none',
-              default='False',
-              help='Display Console information')
-def console(node, display, **kwargs):
-    pass
 
 @click.command()
-@click.argument('node', nargs=-1)
-@click.option('--display/--none',
-              default='False',
-              help='Display Console information')
+@click.argument("node", nargs=1)
+@click.option("--display/--none", default="False", help="Display Console information")
+def console(node, display, **kwargs):
+    """
+    console for node (XXX: currently broken)
+    """
+    server = VIRLServer()
+    client = get_cml_client(server)
+
+    if not node:
+        exit(call(["virl", "console", "--help"]))
+
+    current_lab = get_current_lab()
+    if current_lab:
+        lab = safe_join_existing_lab(current_lab, client)
+        if lab:
+            node_obj = lab.get_node_by_label(node)
+
+            if node_obj.is_active():
+                console = "/{}/{}/0".format(lab.id, node_obj.id)
+                if display:
+                    console_table([{"node": node, "console": console}])
+                else:
+                    # use user specified ssh command
+                    if "CML_CONSOLE_COMMAND" in server.config:
+                        cmd = server.config["CML_CONSOLE_COMMAND"]
+                        cmd = cmd.format(host=server.host, user=server.user, console="open " + console)
+                        print("Calling user specified command: {}".format(cmd))
+                        exit(call(cmd.split()))
+
+                    # someone still uses windows
+                    elif platform.system() == "Windows":
+                        with helpers.disable_file_system_redirection():
+                            cmd = "ssh {}@{} open {}".format(server.user, server.host, console)
+                            exit(call(cmd.split()))
+
+                    # why is shit so complicated?
+                    else:
+                        cmd = "ssh {}@{} open {}".format(server.user, server.host, console)
+                        exit(call(cmd.split()))
+            else:
+                click.secho("Node {} is not active".format(node), fg="red")
+        else:
+            click.secho("Unable to find lab {}".format(current_lab), fg="red")
+    else:
+        click.secho("No current lab set", fg="red")
+
+
+@click.command()
+@click.argument("node", nargs=-1)
+@click.option("--display/--none", default="False", help="Display Console information")
 def console1(node, display, **kwargs):
     """
     console for node
@@ -31,18 +72,18 @@ def console1(node, display, **kwargs):
         node = node[1]
     elif display:
         # only displaying output
-        env = 'default'
+        env = "default"
         running = helpers.check_sim_running(env)
         node = None
 
     elif len(node) == 1:
         # assume default env
-        env = 'default'
+        env = "default"
         running = helpers.check_sim_running(env)
         node = node[0]
     else:
         # node was not specified, display usage
-        exit(call(['virl', 'console', '--help']))
+        exit(call(["virl", "console", "--help"]))
 
     if running:
 
@@ -50,14 +91,13 @@ def console1(node, display, **kwargs):
 
         resp = server.get_node_console(sim_name, node=node)
         if node:
-            click.secho("Attempting to connect to console "
-                        "of {}".format(node))
+            click.secho("Attempting to connect to console " "of {}".format(node))
             try:
-                ip, port = resp.json()[node].split(':')
+                ip, port = resp.json()[node].split(":")
 
                 # use user specified telnet command
-                if 'VIRL_CONSOLE_COMMAND' in server.config:
-                    cmd = server.config['VIRL_CONSOLE_COMMAND']
+                if "VIRL_CONSOLE_COMMAND" in server.config:
+                    cmd = server.config["VIRL_CONSOLE_COMMAND"]
                     cmd = cmd.format(host=ip, port=port)
                     print("Calling user specified command: {}".format(cmd))
                     exit(call(cmd.split()))
@@ -65,16 +105,15 @@ def console1(node, display, **kwargs):
                 # someone still uses windows
                 elif platform.system() == "Windows":
                     with helpers.disable_file_system_redirection():
-                        exit(call(['telnet', ip, port]))
+                        exit(call(["telnet", ip, port]))
 
                 # why is shit so complicated?
                 else:
-                    exit(call(['telnet', ip, port]))
+                    exit(call(["telnet", ip, port]))
             except AttributeError:
-                click.secho("Could not find console info for "
-                            "{}:{}".format(env, node), fg="red")
+                click.secho("Could not find console info for " "{}:{}".format(env, node), fg="red")
             except KeyError:
                 click.secho("Unknown node {}:{}".format(env, node), fg="red")
         else:
             # defaults to displaying table
-            console_table(resp.json())
+            console_table1(resp.json())
--- virl/cli/views/console/console_views.py
+++ virl/cli/views/console/console_views.py
@@ -2,10 +2,29 @@ import tabulate
 import click
 
 
-def console_table(console_entries):
-    click.secho("""
+def console_table(consoles):
+    click.secho("Here is a list of all the running consoles")
+    headers = ["Node", "Console Path"]
+    table = list()
+    for console in consoles:
+        tr = list()
+        tr.append(console["node"])
+        tr.append(console["console"])
+        table.append(tr)
+    # wrap the output in this try/except block as some terminals
+    # may have problem with the 'fancy_grid'
+    try:
+        click.echo(tabulate.tabulate(table, headers, tablefmt="fancy_grid"))
+    except UnicodeEncodeError:
+        click.echo(tabulate.tabulate(table, headers, tablefmt="grid"))
+
+
+def console_table1(console_entries):
+    click.secho(
+        """
     Here is a list of all the running consoles
-    """)
+    """
+    )
     headers = ["Node", "IP", "Port"]
     table = list()
     for node, ip_port in console_entries.items():
