2cdbeb047a3ed1779a05d0ace61246acb9963aa6
Joe Clarke <jclarke@cisco.com>
2020-05-11 08:51:55 -0400

    Add support for generating NSO XML.

--- virl/cli/generate/nso/commands.py
+++ virl/cli/generate/nso/commands.py
@@ -2,26 +2,62 @@ import click
 from virl.api import VIRLServer
 from virl.cli.views import sync_table
 from virl import helpers
-from virl.generators import nso_payload_generator
+from virl.helpers import get_cml_client, safe_join_existing_lab, get_current_lab
+from virl.generators import nso_payload_generator1, nso_payload_generator
 from virl.api.nso import update_devices, perform_sync_from
 
-@click.command()
-@click.argument('env', default='default')
-@click.option('--output', '-o',
-              help="just dump the payload to file without sending")
-@click.option('--syncfrom/--no-syncfrom', default=False,
-              help="Perform sync-from after updating devices")
-# @click.option('--syncto/--no-syncto', default=False,
-#               help="Perform sync-to afgter updating devices")
-def nso(env, syncfrom, **kwargs):
-    pass
 
 @click.command()
-@click.argument('env', default='default')
-@click.option('--output', '-o',
-              help="just dump the payload to file without sending")
-@click.option('--syncfrom/--no-syncfrom', default=False,
-              help="Perform sync-from after updating devices")
+@click.option("--output", "-o", help="just dump the payload to file without sending")
+@click.option("--syncfrom/--no-syncfrom", default=False, help="Perform sync-from after updating devices")
+def nso(syncfrom, **kwargs):
+    """
+    generate nso inventory
+    """
+
+    server = VIRLServer()
+    client = get_cml_client(server)
+
+    current_lab = get_current_lab()
+    if current_lab:
+        lab = safe_join_existing_lab(current_lab, client)
+        if lab:
+
+            if kwargs.get("output"):
+                file_name = kwargs.get("output")
+            else:
+                file_name = None
+
+            inv = nso_payload_generator(lab, server)
+
+            if inv:
+                if file_name:
+                    click.secho("Writing {}".format(file_name))
+                    with open(file_name, "w") as fd:
+                        fd.write(inv)
+                else:
+                    click.secho("Updating NSO....")
+                    nso_response = update_devices(inv)
+                    if nso_response.ok:
+                        click.secho("Successfully added CML devices to NSO")
+                    else:
+                        click.secho("Error updating NSO: ", fg="red")
+                        click.secho(nso_response.text)
+                    if syncfrom:
+                        resp = perform_sync_from()
+                        sync_table(resp.json())
+            else:
+                click.secho("Failed to get inventory data", fg="red")
+        else:
+            click.secho("Failed to find running lab {}".format(current_lab), fg="red")
+    else:
+        click.secho("Current lab is not set", fg="red")
+
+
+@click.command()
+@click.argument("env", default="default")
+@click.option("--output", "-o", help="just dump the payload to file without sending")
+@click.option("--syncfrom/--no-syncfrom", default=False, help="Perform sync-from after updating devices")
 # @click.option('--syncto/--no-syncto', default=False,
 #               help="Perform sync-to afgter updating devices")
 def nso1(env, syncfrom, **kwargs):
@@ -45,14 +81,11 @@ def nso1(env, syncfrom, **kwargs):
         virl_data = server.export(sim_name, ip=True).content
         interfaces = server.get_interfaces(sim_name).json()
 
-        payload = nso_payload_generator(sim_name,
-                                        virl_data,
-                                        roster=roster,
-                                        interfaces=interfaces)
+        payload = nso_payload_generator(sim_name, virl_data, roster=roster, interfaces=interfaces)
 
         if file_name:  # pragma: no cover
             click.secho("Writing payload to {}".format(file_name))
-            with open(file_name, 'w') as payload_file:
+            with open(file_name, "w") as payload_file:
 
                 payload_file.write(payload)
         else:
@@ -61,12 +94,11 @@ def nso1(env, syncfrom, **kwargs):
             if nso_response.ok:
                 click.secho("Successfully added VIRL devices to NSO")
             else:
-                click.secho("Error updating NSO: ", fg='red')
+                click.secho("Error updating NSO: ", fg="red")
                 click.secho(nso_response.text)
             if syncfrom:
                 resp = perform_sync_from()
                 sync_table(resp.json())
 
     else:
-        click.secho("couldnt generate testbed for for env: {}".format(env),
-                    fg='red')
+        click.secho("couldnt generate testbed for for env: {}".format(env), fg="red")
