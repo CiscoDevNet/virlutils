dc9961ecb313d59b05e6cf1e8227269d72f084bb
Joe Clarke <jclarke@cisco.com>
2020-06-01 16:59:45 -0400

    Clarify this string for CML 2+.

--- virl/cli/views/nodes/node_views.py
+++ virl/cli/views/nodes/node_views.py
@@ -3,7 +3,7 @@ import click
 
 
 def node_list_table(nodes):
-    click.secho("Here is a list of the running nodes")
+    click.secho("Here is a list of nodes in this lab")
     table = list()
     headers = ["ID", "Label", "Type", "State", "Wiped?", "L3 Address"]
     skip_types = []
