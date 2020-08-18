8871dd3d470fea82121a92e65e2edf85700ae2b4
Joe Clarke <jclarke@cisco.com>
2020-08-18 06:00:13 -0400

    Use the object approach to patch.

--- tests/generate_nso.py
+++ tests/generate_nso.py
@@ -4,6 +4,7 @@ from .mocks.nso import MockNSOServer
 from click.testing import CliRunner
 import requests_mock
 from virl.cli.main import virl
+
 try:
     from unittest.mock import patch
 except ImportError:
@@ -11,26 +12,24 @@ except ImportError:
 
 
 class Tests(BaseTest):
-
-    @patch('virl.api.nso.NSO.__get_credentials',
-           return_value=('localhost', 'admin', 'admin'))
+    @patch.object("virl.api.nso.NSO", "__get_credentials", return_value=("localhost", "admin", "admin"))
     def test_virl_generate_nso(self, prompt_responses):
         with requests_mock.mock() as m:
             # Mock the request to return what we expect from the API.
-            roster_url = 'http://localhost:19399/roster/rest/'
+            roster_url = "http://localhost:19399/roster/rest/"
             m.get(roster_url, json=MockVIRLServer.get_sim_roster())
-            export_url = 'http://localhost:19399/simengine/rest/export/'
-            export_url += 'TEST_ENV?running-configs=config&updated=true'
+            export_url = "http://localhost:19399/simengine/rest/export/"
+            export_url += "TEST_ENV?running-configs=config&updated=true"
             m.get(export_url, text=MockVIRLServer.export())
-            interface_url = 'http://localhost:19399/simengine/rest/interfaces/'
-            interface_url += 'TEST_ENV'
+            interface_url = "http://localhost:19399/simengine/rest/interfaces/"
+            interface_url += "TEST_ENV"
             m.get(interface_url, json=MockVIRLServer.get_interfaces())
 
             # mock the responses we expect from NSO
-            devices_url = 'http://localhost:8080/api/config/devices/'
+            devices_url = "http://localhost:8080/api/config/devices/"
             m.patch(devices_url, json=MockNSOServer.update_devices())
-            sync_url = 'http://localhost:8080/api/running/devices/'
-            sync_url += '_operations/sync-from'
+            sync_url = "http://localhost:8080/api/running/devices/"
+            sync_url += "_operations/sync-from"
             m.post(sync_url, json=MockNSOServer.perform_sync_from())
             runner = CliRunner()
             result = runner.invoke(virl, ["generate", "nso", "--syncfrom"])
