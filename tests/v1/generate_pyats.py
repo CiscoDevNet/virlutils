b6a9ae83cc11e7053aa12777963a19905b28bf7c
Joe Clarke <jclarke@cisco.com>
2021-05-14 17:13:31 -0400

    Fix testing with new Jinja2.

--- tests/v1/generate_pyats.py
+++ tests/v1/generate_pyats.py
@@ -3,21 +3,21 @@ from .mocks import MockVIRLServer
 from click.testing import CliRunner
 import requests_mock
 from virl.cli.main import virl
+
 # import filecmp
 
 
 class Tests(BaseTest):
-
     def test_virl_generate_pyats(self):
         with requests_mock.mock() as m:
             # Mock the request to return what we expect from the API.
-            roster_url = 'http://localhost:19399/roster/rest/'
+            roster_url = "http://localhost:19399/roster/rest/"
             m.get(roster_url, json=MockVIRLServer.get_sim_roster())
-            export_url = 'http://localhost:19399/simengine/rest/export/'
-            export_url += 'TEST_ENV?running-configs=config&updated=true'
+            export_url = "http://localhost:19399/simengine/rest/export/"
+            export_url += "TEST_ENV?running-configs=config&updated=true"
             m.get(export_url, text=MockVIRLServer.export())
-            interface_url = 'http://localhost:19399/simengine/rest/interfaces/'
-            interface_url += 'TEST_ENV'
+            interface_url = "http://localhost:19399/simengine/rest/interfaces/"
+            interface_url += "TEST_ENV"
             m.get(interface_url, json=MockVIRLServer.get_interfaces())
             runner = CliRunner()
             result = runner.invoke(virl, ["generate", "pyats"])
