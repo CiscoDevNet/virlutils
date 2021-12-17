324dc19527d486b7bfca31fdda958700463bd1bd
Tom Bryan <tombry@cisco.com>
2021-12-17 14:02:14 -0500

    Formatting code with black.
    I also updated CONTRIBUTING.MD to make it easier for future contributors.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -20,6 +20,33 @@ Use the issue tracker to find ways to contribute. Find a bug or a feature, menti
 
 ## Changing the codebase
 
-Generally speaking, you should fork this repository, make changes in your own fork, and then submit a pull request. All new code should have associated unit tests (if applicable) that validate implemented features and the presence or lack of defects.
+Generally speaking, you should fork this repository, make changes in your own fork, and then submit a pull request.
 
-Additionally, the code should follow any stylistic and architectural guidelines prescribed by the project. In the absence of such guidelines, mimic the styles and patterns in the existing codebase.
+## Setting up the development environment
+
+Ensure that you have the minimum required version of Python installed.  See the `python_requires` line in the `./setup.py` file.  To set up your development environment, create a virtual environment, and then use the `pip` to install the requirements and the development requirements.  For example
+
+    $ python3 -mvenv ~/venvs/virlutils-dev/
+    $ source ~/venvs/virlutils-dev/bin/activate
+    (virlutils-dev) $ pip install -r requirements.txt
+    (virlutils-dev) $ pip install -r requirements_dev.txt
+
+### Unit tests
+
+All new code should have associated unit tests (if applicable) that validate implemented features and the presence or lack of defects.  Before you start making changes, check that all existing tests pass.  We recommend using a test-driven development (TDD) to making changes.  Add one or more new unit tests that demonstrate the bug or gap you're trying to fix.  Initially, the test should be failing because you haven't fixed the problem, yet.  Before you submit a pull request, ensure that your new test passes and that all existing tests still pass.
+
+You can run the unit tests from the command line by running `python setup.py tests` from the root directory of the project.  That's currently equivalent to running `python -m unittest discover -v -s ./tests -p '*.py'` from the root directory of the project.
+
+You can also generally configure a Python-aware IDE to run the tests.  For example, if you are using [VSCode](https://code.visualstudio.com/), click **Run > Command Palette > Python: Discover Tests** and chose *unittest*, *tests* folder, and `*.py` pattern.  You can verify the settings by searching your VSCode workspace settings for `Unittest`.  Ensure that the checkbox under **Python > Testing: Unittest Enabled** is checked and that the **Python > Testing: Unittest Args** are
+
+    -v
+    -s
+    ./tests
+    -p
+    *.py
+
+With those settings, navigate to **View > Testing** in VSCode.  The view should list all of the test methods discovered under the `tests` folder.  You may need to **Refresh Tests** first.  Because the tests use mock responses, you can run them without access to a CML server.
+
+### Code Style
+
+Additionally, the code should follow any stylistic and architectural guidelines prescribed by the project.  The project now uses [black](https://black.readthedocs.io/) to ensure consistent code formatting.  When you installed the requirements_dev.txt in your virtual environment, it installed the `black` command.  For each file that you are modifying, run `black ./virl/path/to/file.py` before you commit the file or submit a pull request.
--- tests/v2/definitions.py
+++ tests/v2/definitions.py
@@ -29,23 +29,15 @@ class CMLDefinitionsTest(BaseCMLTest):
 
     def test_node_definitions_list(self):
         with requests_mock.Mocker() as m:
-            with open(os.path.join(os.path.dirname(__file__), "static/response_get_node_defs.json"), 'rb') as fh_node_defs:
+            with open(os.path.join(os.path.dirname(__file__), "static/response_get_node_defs.json"), "rb") as fh_node_defs:
                 # Mock the request to return what we expect from the API.
                 self.setup_mocks(m)
                 m.get(
-                    self.get_api_path("node_definitions/"),
-                    body=fh_node_defs,
-                    headers={"content-type": "application/json; charset=utf-8"})
+                    self.get_api_path("node_definitions/"), body=fh_node_defs, headers={"content-type": "application/json; charset=utf-8"}
+                )
                 virl = self.get_virl()
                 runner = CliRunner()
-                result = runner.invoke(
-                    virl,
-                    [
-                        "definitions",
-                        "nodes",
-                        "ls"
-                    ]
-                )
+                result = runner.invoke(virl, ["definitions", "nodes", "ls"])
                 if result.exception:
                     traceback.print_exception(*result.exc_info)
                     raise result.exception
@@ -55,35 +47,27 @@ class CMLDefinitionsTest(BaseCMLTest):
 
     def test_node_definitions_list_one(self):
         with requests_mock.Mocker() as m:
-            with open(os.path.join(os.path.dirname(__file__), "static/response_get_node_defs.json"), 'rb') as fh_node_defs:
+            with open(os.path.join(os.path.dirname(__file__), "static/response_get_node_defs.json"), "rb") as fh_node_defs:
                 # Mock the request to return what we expect from the API.
                 self.setup_mocks(m)
                 m.get(
-                    self.get_api_path("node_definitions/"),
-                    body=fh_node_defs,
-                    headers={"content-type": "application/json; charset=utf-8"})
+                    self.get_api_path("node_definitions/"), body=fh_node_defs, headers={"content-type": "application/json; charset=utf-8"}
+                )
                 virl = self.get_virl()
                 runner = CliRunner()
-                result = runner.invoke(
-                    virl,
-                    [
-                        "definitions",
-                        "nodes",
-                        "ls",
-                        "--node",
-                        "nxosv9000"
-                    ]
-                )
+                result = runner.invoke(virl, ["definitions", "nodes", "ls", "--node", "nxosv9000"])
                 if result.exception:
                     traceback.print_exception(*result.exc_info)
                     raise result.exception
-                expected = textwrap.dedent("""\
+                expected = textwrap.dedent(
+                    """\
                     ╒═══════════╤════════════╤══════════════════════════╤══════════════════════╤════════╤════════╤══════════════════╕
                     │ ID        │ Label      │ Description              │   Max No. Interfaces │ RAM    │   CPUs │ Boot Disk Size   │
                     ╞═══════════╪════════════╪══════════════════════════╪══════════════════════╪════════╪════════╪══════════════════╡
                     │ nxosv9000 │ NX-OS 9000 │ Cisco Nexus 9000v Switch │                   65 │ 8.0 GB │      2 │ N/A              │
                     ╘═══════════╧════════════╧══════════════════════════╧══════════════════════╧════════╧════════╧══════════════════╛
-                    """)
+                    """
+                )
                 self.assertEquals(expected, result.output)
 
     def test_node_definitions_list_legacy(self):
@@ -92,23 +76,15 @@ class CMLDefinitionsTest(BaseCMLTest):
         returned by the API before CML 2.3.
         """
         with requests_mock.Mocker() as m:
-            with open(os.path.join(os.path.dirname(__file__), "static/response_get_node_defs_cml22.json"), 'rb') as fh_node_defs:
+            with open(os.path.join(os.path.dirname(__file__), "static/response_get_node_defs_cml22.json"), "rb") as fh_node_defs:
                 # Mock the request to return what we expect from the API.
                 self.setup_mocks(m)
                 m.get(
-                    self.get_api_path("node_definitions/"),
-                    body=fh_node_defs,
-                    headers={"content-type": "application/json; charset=utf-8"})
+                    self.get_api_path("node_definitions/"), body=fh_node_defs, headers={"content-type": "application/json; charset=utf-8"}
+                )
                 virl = self.get_virl()
                 runner = CliRunner()
-                result = runner.invoke(
-                    virl,
-                    [
-                        "definitions",
-                        "nodes",
-                        "ls"
-                    ]
-                )
+                result = runner.invoke(virl, ["definitions", "nodes", "ls"])
                 if result.exception:
                     traceback.print_exception(*result.exc_info)
                     raise result.exception
