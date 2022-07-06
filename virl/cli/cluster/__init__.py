83b9ae9fb719b1dfdf5da237ea30b08ff4877807
Joe Clarke <jclarke@cisco.com>
2022-07-05 23:04:03 -0400

    Add cluster command stubs.

---
+++ virl/cli/cluster/__init__.py
@@ -0,0 +1,13 @@
+import click
+from virl.cli.cluster.info.commands import info
+
+
+@click.group()
+def cluster():
+    """
+    display and manage CML cluster details
+    """
+    pass
+
+
+cluster.add_command(info)
---
+++ virl/cli/cluster/info/commands.py
@@ -0,0 +1,34 @@
+import click
+from virl.api import VIRLServer, ViewerPlugin, NoPluginError
+from virl.cli.views import node_def_list_table
+from virl.helpers import get_cml_client
+
+
+@click.command()
+def info():
+    """
+    display cluster configuratioh details
+    """
+
+    server = VIRLServer()
+    client = get_cml_client(server)
+    pl = None
+
+    system_health = None
+    try:
+        response = client.session.get(client._base_url + "system_information")
+        response.raise_for_status()
+        system_health = response.json()
+    except Exception as e:
+        click.secho(f"Failed to get system health data: {e} ({response.text})", fg="red")
+        exit(1)
+
+    try:
+        pl = ViewerPlugin(viewer="cluster")
+    except NoPluginError:
+        pass
+
+    if pl:
+        pl.visualize(nodes=system_health["computes"])
+    else:
+        node_def_list_table(system_health["computes"])
