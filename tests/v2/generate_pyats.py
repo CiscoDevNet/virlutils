f085968eddeb26e44aa17d615d7131e1b6981d4d
Salah Gherdaoui <sgherdao@cisco.com>
2023-11-02 18:22:10 +0000

    new test for generate pyats and new mock

---
+++ tests/v2/generate_pyats.py
@@ -0,0 +1,22 @@
+from . import BaseCMLTest
+from click.testing import CliRunner
+
+
+class Tests(BaseCMLTest):
+    def test_virl_generate_pyats(self):
+        with self.get_context() as m:
+            # Mock the request to return what we expect from the API.
+            self.setup_mocks(m)
+            virl = self.get_virl()
+            runner = CliRunner()
+            result = runner.invoke(virl, ["generate", "pyats"])
+            self.assertEqual(0, result.exit_code)
+
+    def test_virl_generate_pyats_topology(self):
+        with self.get_context() as m:
+            # Mock the request to return what we expect from the API.
+            self.setup_mocks(m)
+            virl = self.get_virl()
+            runner = CliRunner()
+            result = runner.invoke(virl, ["generate", "pyats", "-o", "topology.yaml"])
+            self.assertEqual(0, result.exit_code)
