59f7864ffab629abf7f066d9017da501e6724d2c
Joe Clarke <jclarke@cisco.com>
2025-03-01 17:54:50 -0500

    Add support for passing node ID to start and stop.

--- virl/cli/start/commands.py
+++ virl/cli/start/commands.py
@@ -1,17 +1,21 @@
 import click
+from subprocess import call
 from virl2_client.exceptions import NodeNotFound
 
 from virl.api import VIRLServer
-from virl.helpers import (get_cml_client, get_current_lab,
-                          safe_join_existing_lab)
+from virl.helpers import get_cml_client, get_current_lab, safe_join_existing_lab, get_command
 
 
 @click.command()
-@click.argument("node", nargs=1)
+@click.argument("node", required=False)
+@click.option("--id", required=False, help="An existing node ID to start (the node name argument is ignored)")
 def start(node):
     """
     start a node
     """
+    if not node and not id:
+        exit(call([get_command(), "start", "--help"]))
+
     server = VIRLServer()
     client = get_cml_client(server)
 
@@ -20,7 +24,10 @@ def start(node):
         lab = safe_join_existing_lab(current_lab, client)
         if lab:
             try:
-                node_obj = lab.get_node_by_label(node)
+                if id:
+                    node_obj = lab.get_node_by_id(id)
+                else:
+                    node_obj = lab.get_node_by_label(node)
 
                 if not node_obj.is_active():
                     node_obj.start(wait=True)
@@ -28,7 +35,7 @@ def start(node):
                 else:
                     click.secho("Node {} is already active".format(node_obj.label), fg="yellow")
             except NodeNotFound:
-                click.secho("Node {} was not found in lab {}".format(node, current_lab), fg="red")
+                click.secho("Node {} was not found in lab {}".format(id if id else node, current_lab), fg="red")
                 exit(1)
         else:
             click.secho("Unable to find lab {}".format(current_lab), fg="red")
--- virl/cli/stop/commands.py
+++ virl/cli/stop/commands.py
@@ -1,17 +1,22 @@
 import click
+from subprocess import call
+
 from virl2_client.exceptions import NodeNotFound
 
 from virl.api import VIRLServer
-from virl.helpers import (get_cml_client, get_current_lab,
-                          safe_join_existing_lab)
+from virl.helpers import get_cml_client, get_current_lab, safe_join_existing_lab, get_command
 
 
 @click.command()
-@click.argument("node", nargs=1)
+@click.argument("node", required=False)
+@click.option("--id", required=False, help="An existing node ID to stop (the node name argument is ignored)")
 def stop(node):
     """
     stop a node
     """
+    if not node and not id:
+        exit(call([get_command(), "stop", "--help"]))
+
     server = VIRLServer()
     client = get_cml_client(server)
 
@@ -20,7 +25,10 @@ def stop(node):
         lab = safe_join_existing_lab(current_lab, client)
         if lab:
             try:
-                node_obj = lab.get_node_by_label(node)
+                if id:
+                    node_obj = lab.get_node_by_id(id)
+                else:
+                    node_obj = lab.get_node_by_label(node)
 
                 if node_obj.is_active():
                     node_obj.stop(wait=True)
@@ -28,7 +36,7 @@ def stop(node):
                 else:
                     click.secho("Node {} is already stopped".format(node_obj.label), fg="yellow")
             except NodeNotFound:
-                click.secho("Node {} was not found in lab {}".format(node, current_lab), fg="red")
+                click.secho("Node {} was not found in lab {}".format(id if id else node, current_lab), fg="red")
                 exit(1)
         else:
             click.secho("Unable to find lab {}".format(current_lab), fg="red")
