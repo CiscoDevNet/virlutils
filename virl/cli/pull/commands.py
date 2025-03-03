e5774b44cafae98b0c0db264a69b55e23cf6d64e
Joe Clarke <jclarke@cisco.com>
2025-03-03 12:52:00 -0500

    Add support for flexible files and branches.

--- virl/cli/pull/commands.py
+++ virl/cli/pull/commands.py
@@ -3,7 +3,7 @@ import requests
 
 
 def do_pull(repo, fname, branch="master", recurse=False):
-    click.secho("Pulling from {} on branch {}".format(repo, branch))
+    click.secho("Pulling {} from {} on branch {}".format(fname, repo, branch))
     url = "https://raw.githubusercontent.com/"
     url = url + "{}/{}/{}".format(repo, branch, fname)
     resp = requests.get(url)
@@ -12,19 +12,19 @@ def do_pull(repo, fname, branch="master", recurse=False):
             fh.write(resp.text)
         click.secho("Saved topology as {}".format(fname), fg="green")
         return True
-    elif resp.status_code == 404 and not recurse:
-        return do_pull(repo, fname, "main", True)
     else:
-        click.secho("Error pulling {} - repo or file not found".format(repo), fg="red")
+        click.secho("Error pulling {} form {} on branch {} - repo, file, or branch not found".format(fname, repo, branch), fg="red")
         return False
 
 
 @click.command()
 @click.argument("repo")
-def pull(repo):
+@click.option("--file", default="topology.yaml", required=False, help="Filename to pull (default: topology.yaml)")
+@click.option("--branch", default="main", required=False, help="Branch name from which to pull (default: main)")
+def pull(repo, file, branch):
     """
     pull topology.yaml from repo
     """
-    ret = do_pull(repo, "topology.yaml")
+    ret = do_pull(repo, fname=file, branch=branch)
     if not ret:
         exit(1)
